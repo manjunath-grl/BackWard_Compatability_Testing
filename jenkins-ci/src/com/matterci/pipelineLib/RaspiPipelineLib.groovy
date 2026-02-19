6bcb7fde44b140923aa6d7b2f295c0f49a9506b4
manjunath-grl <manjunath@graniteriverlabs.in>
2026-02-19 22:35:11 +0530

    Fixes all ready

--- jenkins-ci/src/com/matterci/pipelineLib/RaspiPipelineLib.groovy
+++ jenkins-ci/src/com/matterci/pipelineLib/RaspiPipelineLib.groovy
@@ -154,7 +154,7 @@ class RaspiPipelineLib implements Serializable {
                     steps.echo "raspi workspace on device node is ${deviceRaspiWorkspace}"
                     steps.ws("${deviceRaspiWorkspace}") {
                         def jfrogRepoName = testConfigs.ci_config.jfrog_repo_name
-                        def sourcePath = "${jfrogRepoName}/${projectName}/${buildNumber}/${deviceRaspiWorkspace}/"
+                        def sourcePath = "${jfrogRepoName}/${projectName}/${BUILD_NUMBER}/${deviceRaspiWorkspace}/"
                         steps.echo "Downloading from Artifactory path: ${sourcePath}"
 
                         steps.jf """

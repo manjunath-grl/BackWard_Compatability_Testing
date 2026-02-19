481fb22c9d0dbe5b4239184c28141571e90df43a
manjunath-grl <manjunath@graniteriverlabs.in>
2026-02-19 21:07:22 +0530

    Basic error fix for J-frog

--- jenkins-ci/vars/raspiPipeline.groovy
+++ jenkins-ci/vars/raspiPipeline.groovy
@@ -334,7 +334,7 @@ def buildApps(testConfigs, testCasesList, workSpace, raspiBinariesDir){
 
 }
 
-def call(steps, testConfigs, testCasesList) {
+def call(testConfigs, testCasesList) {
     def buildSuccess = true
     def raspiStages = testConfigs.ci_config?.raspi_pipeline?.stages
     def copyBuildArtifact = testConfigs.ci_config?.copy_build_artifact
@@ -343,8 +343,6 @@ def call(steps, testConfigs, testCasesList) {
     def controllerBuildWorkSpace = ''
     def appsBuildWorkSpace = ''
     def logTransferConfig = testConfigs.execution_log_transfer_config
-
-    def server = steps.Artifactory.server('artifactory-oss')
 
     //TODO: Not scalable, Fix this code to download cloned code in the above step
     if (raspiStages?.build_firmware?.enabled){

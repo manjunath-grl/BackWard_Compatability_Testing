15f29c0112bf27ab29f63bf3540cd3b8c35a1775
manjunath-grl <manjunath@graniteriverlabs.in>
2026-03-04 21:39:27 +0530

    fix

--- jenkins-ci/vars/raspiPipeline.groovy
+++ jenkins-ci/vars/raspiPipeline.groovy
@@ -94,8 +94,8 @@ def buildAndinstallControllerBinaries(def steps,testConfigs, workSpace, raspiBin
         def sdkCfg  = testConfigs.ci_config?.clone_sdk_code_stage?.controller_sdk_config
         def repoUrl = sdkCfg?.repoUrl
         def branch  = sdkCfg?.branch
-        def raspiDecision = decision.platforms["raspi"]
         def decision = testConfigs.ci_config.artifactDecision
+        def raspiDecision = decision.platforms["raspi"]
 
         def hostname = sh(script: "hostname", returnStdout: true).trim()
         echo "hostname : ${hostname}"

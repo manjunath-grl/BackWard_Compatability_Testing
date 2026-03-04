77b26a476f895f232a4c8a10e8868a4004a690a1
manjunath-grl <manjunath@graniteriverlabs.in>
2026-03-04 23:27:02 +0530

    error

--- jenkins-ci/vars/raspiPipeline.groovy
+++ jenkins-ci/vars/raspiPipeline.groovy
@@ -371,6 +371,10 @@ def call(testConfigs, testCasesList) {
     def platformCfg = testConfigs.ci_config.clone_sdk_code_stage.platforms.raspi
     def appName = platformCfg.app_to_test ?: testConfigs.ci_config.app_to_test
 
+    echo "Controller repo : ${testConfigs?.ci_config?.clone_sdk_code_stage?.controller_sdk_config?.controller_repo == 'certification-tool'}"
+    echo "Controller missing : ${raspiDecision.controllerMissing}"
+    error "Apps missing : ${raspiDecision.appsMissing}"
+
     //TODO: Not scalable, Fix this code to download cloned code in the above step
     //if (raspiStages?.build_firmware?.enabled){
     if (raspiDecision?.controllerMissing || raspiDecision?.appsMissing) {
